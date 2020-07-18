6d9207d8e26028a6a8ae6593fb24e6ce3c0d04e1
unknown <luismauboy.lm@gmail.com>
2020-07-19 02:27:40 +0800

    Fix & Foundation

--- GrafkomG-SA2020-TR1/GrafkomG-SA2020-TR1/Source.cpp
+++ GrafkomG-SA2020-TR1/GrafkomG-SA2020-TR1/Source.cpp
@@ -1,125 +1,137 @@
-#include <stdlib.h>
+#include <windows.h>
 #include <GL/glut.h>
-static float ypoz = 0, zpoz = 0, xpoz = 0, a = 0, b = 0, c = -10;
-void mall(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int
-	z1, float z2, float z3) {
-	glColor3f(0, 0, 0); //sisi depan
-	glVertex3d(x1, y1, z2);
-	glVertex3d(x2, y2, z2);
-	glVertex3d(x3, y3, z2);
-	glVertex3d(x4, y4, z2);
-	glColor3f(0, 0, 0);//right
-	glVertex3d(x2, y2, z2);
-	glVertex3d(x2, y2, z1);
-	glVertex3d(x3, y3, z1);
-	glVertex3d(x3, y3, z2);
-	glColor3f(0, 0, 0);//back
-	glVertex3d(x1, y1, z1);
-	glVertex3d(x2, y2, z1);
-	glVertex3d(x3, y3, z1);
-	glVertex3d(x4, y4, z1);
-	glColor3f(0, 0, 0);//left
-	glVertex3d(x1, y1, z2);
-	glVertex3d(x1, y1, z1);
-	glVertex3d(x4, y4, z1);
-	glVertex3d(x4, y4, z2);
-	glColor3f(0, 0, 0);//bottom
-	glVertex3d(x1, y1, z2);
-	glVertex3d(x2, y2, z2);
-	glVertex3d(x2, y2, z1);
-	glVertex3d(x1, y1, z1);
-	glColor3f(0, 0, 0);//top
-	glVertex3d(x3, y3, z2);
-	glVertex3d(x4, y4, z2);
-	glVertex3d(x4, y4, z1);
-	glVertex3d(x3, y3, z1);
-	glEnd();
-}
 
+int y1 = 20;
+void init(void);
+void display(void);
+void keyboard(unsigned char, int, int);
+void shape(int, int);
 
+int is_depth;
 
-void init(void)
-{
-	glClearColor(1, 1, 1, 1);
-	glOrtho(-12, 12, -6, 6, -35, 35);
-	glEnable(GL_DEPTH_TEST);
-	glShadeModel(GL_SMOOTH);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(80.0, 2.2, 1.0, 20.0);
-	glMatrixMode(GL_MODELVIEW);
-}
-void display(void)
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	glTranslatef(a, b, c); 
-	glRotatef(xpoz, 1, 0, 0);
-	glRotatef(ypoz, 0, 1, 0);
-	glRotatef(zpoz, 0, 0, 1);
-	glBegin(GL_QUADS);
-	mall(-8, -2, 8, -2, 8, 2, -8, 2, -5, 3, 0);
-	glutSwapBuffers(); 
-	
-		
-}
-void keyboard(unsigned char key, int x, int y)
-{
-	switch (key) {
-
-	case 'x':
-		xpoz = xpoz + 5;
-		if (xpoz > 360) xpoz = 0;
-		glutPostRedisplay();
-		break;
-	case 'y':
-		ypoz = ypoz + 5;
-		if (ypoz > 360) ypoz = 0;
-		glutPostRedisplay();
-		break;
-	case 'z':
-		zpoz = zpoz + 1;
-		if (zpoz > 360) zpoz = 0;
-
-		glutPostRedisplay();
-		break;
-	case 'w':
-		b = b + 1;
-		glutPostRedisplay();
-		break;
-	case 's':
-		b = b - 1;
-		glutPostRedisplay();
-		break;
-	case 'a':
-		a = a + 1;
-		glutPostRedisplay();
-		break;
-	case 'd':
-		a = a - 1;
-		glutPostRedisplay();
-		break;
-	case 'q':
-		c = c + 1;
-		glutPostRedisplay();
-		break;
-	case 'e':
-		c = c - 1;
-		glutPostRedisplay();
-		break;
-
-	}
-}
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("TSM Bandung");
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_MULTISAMPLE);
+	glutInitWindowSize(1920, 1080);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("TSM-Bandung");
 	init();
 	glutDisplayFunc(display);
 	glutKeyboardFunc(keyboard);
+	glutReshapeFunc(shape);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_LINE_SMOOTH);
+	glEnable(GL_POINT_SMOOTH);
+	glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+	glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
 	glutMainLoop();
 	return 0;
+}
+
+void init(void) {
+	glClearColor(255, 255, 255, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glEnable(GL_DEPTH_TEST);
+	is_depth = 1;
+	glMatrixMode(GL_MODELVIEW);
+	glPointSize(9.0);
+	glLineWidth(1.0f);
+	glLineWidth(3.0f);
+}
+
+
+void display(void) {
+	if (is_depth)
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	else
+		glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3ub(0, 0, 0);
+
+	glBegin(GL_POLYGON);
+	glVertex3d(-77.95, y1, 20.24);
+	glVertex3d(-60.64, y1, 36.13);
+	glVertex3d(3.41, y1, 36.13);
+	glVertex3d(3.41, y1, 25.47);
+	glVertex3d(44.70, y1, 25.47);
+	glVertex3d(55.02, y1, 16.61);
+	glVertex3d(55.02, y1, -4.20);
+	glVertex3d(44.96, y1, -4.20);
+	glVertex3d(44.96, y1, -5.78);
+	glVertex3d(40.80, y1, -5.78);
+	glVertex3d(40.80, y1, -8.66);
+	glVertex3d(17.48, y1, -8.66);
+	glVertex3d(17.48, y1, -2.91);
+	glVertex3d(3.34, y1, -2.91);
+	glVertex3d(3.29, y1, -38.25);
+	glVertex3d(-77.95, y1, -38.25);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(55.02, y1, 25.47);
+	glVertex3d(88.75, y1, 36.52);
+	glVertex3d(99.41, y1, 8.77);
+	glVertex3d(55.62, y1, -5.78);
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case '3':
+		glTranslatef(3.0, 0.0, 0.0);
+		break;
+	case '1':
+		glTranslatef(-3.0, 0.0, 0.0);
+		break;
+
+	case 's':
+	case 'S':
+		glRotatef(2.0, 1.0, 0.0, 0.0);
+		break;
+	case 'x':
+	case 'X':
+		glRotatef(-2.0, 1.0, 0.0, 0.0);
+		break;
+	case 'd':
+	case 'D':
+		glRotatef(2.0, 0.0, 1.0, 0.0);
+		break;
+	case 'c':
+	case 'C':
+		glRotatef(-2.0, 0.0, 1.0, 0.0);
+		break;
+	case 'a':
+	case 'A':
+		glRotatef(2.0, 0.0, 0.0, 1.0);
+		break;
+	case 'z':
+	case 'Z':
+		glRotatef(-2.0, 0.0, 0.0, 1.0);
+		break;
+
+
+	case 'q':
+	case 'Q':
+		glScalef(0.8, 0.8, 0.8f);
+		break;
+	case 'e':
+	case 'E':
+		glScalef(1.2, 1.2, 1.2f);
+		break;
+	}
+	display();
+}
+
+
+void shape(int width, int height) {
+	if (height == 0) height = 1;
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(50.0, width / height, 5.0, 1000.0);
+	glTranslatef(0.0, -5.0, -400.0);
+	glMatrixMode(GL_MODELVIEW);
 }
