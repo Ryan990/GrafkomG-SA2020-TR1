a070299b96a093aad13587a9fcd08471e3d3f9df
Chrys-N <60644479+Chrys-N@users.noreply.github.com>
2020-07-19 20:25:29 +0700

    Add files via upload
    pohon &awan

--- GrafkomG-SA2020-TR1/GrafkomG-SA2020-TR1/Source.cpp
+++ GrafkomG-SA2020-TR1/GrafkomG-SA2020-TR1/Source.cpp
@@ -7,6 +7,9 @@ int y3 = y2 + 5;
 void init(void);
 void wall(void);
 void display(void);
+void awan(void);
+void pohon(void);
+void ranting(void);
 void keyboard(unsigned char, int, int);
 void shape(int, int);
 void Timer(int);
@@ -197,6 +200,65 @@ void wall() {
 
 }
 
+void pohon() {
+	//batang
+	GLUquadricObj* pObj;
+	pObj = gluNewQuadric();
+	gluQuadricNormals(pObj, GLU_SMOOTH);
+
+	glPushMatrix();
+	glColor3ub(104, 70, 14);
+	glRotatef(270, 1, 0, 0);
+	gluCylinder(pObj, 4, 0.7, 30, 25, 25);
+	glPopMatrix();
+}
+
+
+//ranting
+void ranting() {
+	GLUquadricObj* pObj;
+	pObj = gluNewQuadric();
+	gluQuadricNormals(pObj, GLU_SMOOTH);
+	glPushMatrix();
+	glColor3ub(104, 70, 14);
+	glTranslatef(0, 27, 0);
+	glRotatef(330, 1, 0, 0);
+	gluCylinder(pObj, 0.6, 0.1, 15, 25, 25);
+	glPopMatrix();
+
+	//daun
+	glPushMatrix();
+	glColor3ub(18, 118, 13);
+	glScaled(5, 5, 5);
+	glTranslatef(0, 7, 3);
+	glutSolidDodecahedron();
+	glPopMatrix();
+}
+
+void awan() {
+	glPushMatrix();
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	glColor3ub(153, 223, 255);
+	glutSolidSphere(10, 50, 50);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(10, 0, 1);
+	glutSolidSphere(5, 50, 50);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-2, 6, -2);
+	glutSolidSphere(7, 50, 50);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-10, -3, 0);
+	glutSolidSphere(7, 50, 50);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(6, -2, 2);
+	glutSolidSphere(7, 50, 50);
+	glPopMatrix();
+}
+
 void display(void) {
 	if (is_depth)
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -286,13 +348,13 @@ void display(void) {
 
 	glPushMatrix(); // matahari
 	glEnable(GL_COLOR_MATERIAL);
-	glTranslatef(-150 + x_pos ,160 ,10);
+	glTranslatef(-150 + x_pos, 160, 10);
 	glRotatef(sudut, xx, yy, zz);
 	glColor3f(1, 1, 0);
 	glutSolidSphere(2, 20, 20);
 	glDisable(GL_COLOR_MATERIAL);
 	glPopMatrix();
-	
+
 	glBegin(GL_LINES); // garis hitam bak sampah
 	glColor3d(0, 0, 0);
 	glVertex3d(7.20, y1, 34.13);
@@ -315,7 +377,7 @@ void display(void) {
 	glVertex3d(3.41, y1, 25.47);
 	glVertex3d(3.41, y1 + 8, 25.47);
 	glVertex3d(3.41, y1 + 8, 34.13);
-	glVertex3d(7.20, y1, 34.13); 
+	glVertex3d(7.20, y1, 34.13);
 	glVertex3d(7.20, y1, 25.47);
 	glVertex3d(7.20, y1 + 8, 25.47);
 	glVertex3d(7.20, y1 + 8, 34.13);
@@ -353,7 +415,7 @@ void display(void) {
 	glVertex3d(7.20, y1 + 8, 25.47);
 	glVertex3d(7.20, y1 + 8, 34.13);
 	glEnd();
-	
+
 	////// TUTUP BAK SAMPAH
 	glBegin(GL_POLYGON);
 	glColor3f(0, 0, 0);
@@ -387,7 +449,7 @@ void display(void) {
 	glVertex3d(7.20, y1 + 8.5, 25.47);
 	glVertex3d(7.20, y1 + 8.5, 34.13);
 	glEnd();
-	
+
 	glBegin(GL_POLYGON); // Motif bak sampah
 	glColor3d(0, 0.5, 0);
 	glVertex3d(3.61, y1 + 8.5, 34.13); // atas
@@ -453,6 +515,196 @@ void display(void) {
 	glVertex3d(7.00, y1 + 8.7, 26.00);
 	glVertex3d(7.00, y1 + 8.7, 28.77);
 	glEnd();
+
+	//pohon1
+	glPushMatrix();
+	glTranslatef(-50, 0, 50);
+	glScalef(0.2, 0.2, 0.2);
+	glRotatef(90, 0, 1, 0);
+	pohon();
+	//ranting1
+	ranting();
+	//ranting2
+	glPushMatrix();
+	glScalef(1.5, 1.5, 1.5);
+	glTranslatef(0, 25, 25);
+	glRotatef(250, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	//ranting3
+	glPushMatrix();
+	glScalef(1.8, 1.8, 1.8);
+	glTranslatef(0, -6, 21.5);
+	glRotatef(-55, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	glPopMatrix();
+
+	//pohon2
+	glPushMatrix();
+	glTranslatef(-80, 0, 50);
+	glScalef(0.2, 0.2, 0.2);
+	glRotatef(90, 0, 1, 0);
+	pohon();
+	//ranting1
+	ranting();
+	//ranting2
+	glPushMatrix();
+	glScalef(1.5, 1.5, 1.5);
+	glTranslatef(0, 25, 25);
+	glRotatef(250, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	//ranting3
+	glPushMatrix();
+	glScalef(1.8, 1.8, 1.8);
+	glTranslatef(0, -6, 21.5);
+	glRotatef(-55, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	glPopMatrix();
+
+	//pohon3
+	glPushMatrix();
+	glTranslatef(-20, 0, 50);
+	glScalef(0.2, 0.2, 0.2);
+	glRotatef(90, 0, 1, 0);
+	pohon();
+	//ranting1
+	ranting();
+	//ranting2
+	glPushMatrix();
+	glScalef(1.5, 1.5, 1.5);
+	glTranslatef(0, 25, 25);
+	glRotatef(250, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	//ranting3
+	glPushMatrix();
+	glScalef(1.8, 1.8, 1.8);
+	glTranslatef(0, -6, 21.5);
+	glRotatef(-55, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	glPopMatrix();
+
+	//pohon4
+	glPushMatrix();
+	glTranslatef(10, 0, 50);
+	glScalef(0.2, 0.2, 0.2);
+	glRotatef(90, 0, 1, 0);
+	pohon();
+	//ranting1
+	ranting();
+	//ranting2
+	glPushMatrix();
+	glScalef(1.5, 1.5, 1.5);
+	glTranslatef(0, 25, 25);
+	glRotatef(250, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	//ranting3
+	glPushMatrix();
+	glScalef(1.8, 1.8, 1.8);
+	glTranslatef(0, -6, 21.5);
+	glRotatef(-55, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	glPopMatrix();
+
+	//pohon5
+	glPushMatrix();
+	glTranslatef(40, 0, 50);
+	glScalef(0.2, 0.2, 0.2);
+	glRotatef(90, 0, 1, 0);
+	pohon();
+	//ranting1
+	ranting();
+	//ranting2
+	glPushMatrix();
+	glScalef(1.5, 1.5, 1.5);
+	glTranslatef(0, 25, 25);
+	glRotatef(250, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	//ranting3
+	glPushMatrix();
+	glScalef(1.8, 1.8, 1.8);
+	glTranslatef(0, -6, 21.5);
+	glRotatef(-55, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	glPopMatrix();
+
+	//pohon6
+	glPushMatrix();
+	glTranslatef(70, 0, 50);
+	glScalef(0.2, 0.2, 0.2);
+	glRotatef(90, 0, 1, 0);
+	pohon();
+	//ranting1
+	ranting();
+	//ranting2
+	glPushMatrix();
+	glScalef(1.5, 1.5, 1.5);
+	glTranslatef(0, 25, 25);
+	glRotatef(250, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	//ranting3
+	glPushMatrix();
+	glScalef(1.8, 1.8, 1.8);
+	glTranslatef(0, -6, 21.5);
+	glRotatef(-55, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	glPopMatrix();
+
+	//pohon7
+	glPushMatrix();
+	glTranslatef(100, 0, 50);
+	glScalef(0.2, 0.2, 0.2);
+	glRotatef(90, 0, 1, 0);
+	pohon();
+	//ranting1
+	ranting();
+	//ranting2
+	glPushMatrix();
+	glScalef(1.5, 1.5, 1.5);
+	glTranslatef(0, 25, 25);
+	glRotatef(250, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	//ranting3
+	glPushMatrix();
+	glScalef(1.8, 1.8, 1.8);
+	glTranslatef(0, -6, 21.5);
+	glRotatef(-55, 1, 0, 0);
+	ranting();
+	glPopMatrix();
+	glPopMatrix();
+
+	//awan
+	glPushMatrix();
+	glTranslatef(-130 + x_pos, 140, 35);
+	glScalef(0.2, 0.2, 1.0);
+	awan();
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-105 + x_pos, 160, 30);
+	glScalef(0.2, 0.2, 1.0);
+	awan();
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-150 + x_pos, 136, 25);
+	glScalef(0.2, 0.2, 1.0);
+	awan();
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-160 + x_pos, 145, 45);
+	glScalef(0.2, 0.2, 1.0);
+	awan();
+	glPopMatrix();
 
 	glutSwapBuffers();
 }
